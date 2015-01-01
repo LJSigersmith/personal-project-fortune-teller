039e5f504172560bd8f7d233787a6136f8a96b50
LJ Sigersmith <LJ@LJs-MacBook-Pro.local>
2015-01-01 17:30:29 -0500

    Initial Commit

---
+++ Buttons Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Buttons Game
+//
+//  Created by LJ Sigersmith on 1/1/15.
+//  Copyright (c) 2015 LJ Sigersmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
